e64e7d80783875db3db1c43e16c65e1916c474f3
vikashchandra458 <vikashchandra458@gmail.com>
2022-09-22 14:57:43 +0530

    abcd

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   createStackNavigator,
   TransitionPresets,
 } from "@react-navigation/stack";
+// import "react-native-gesture-handler";
 import LoginProcess from "./views/login/loginProcess";
 import IntroSlides from "./views/introSlides";
 import LandingPage from "./views/landingPage/landingPage";
@@ -10,11 +11,14 @@ import ConfirmEmailProcess from "./views/register/confirmEmail/confirmEmailProce
 import SetPasswordProcess from "./views/register/setPassword/setPasswordProcess";
 import VerifyOTP from "./views/register/verifyOTP/verifyOTP";
 import SetUpPin from "./views/register/setUpPin/setUpPin";
+import AccountLevel from "./views/accountLevel/accountLevel";
+import CompleteProfileProcess1 from "./views/completeProfile/completeProfile1/completeProfileProcess1";
+import CompleteProfileProcess2 from "./views/completeProfile/completeProfile2/completeProfileProcess2";
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
+  <NavigationContainer theme={{ colors: { background: "#270041" } }}>
       <Stack.Navigator initialRouteName="introSlides">
         <Stack.Screen
           name="introSlides"
@@ -95,6 +99,42 @@ export default function App() {
           }}
           component={SetUpPin}
         />
+        <Stack.Screen
+          name="accountLevel"
+          options={{
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: "#F2F6FF",
+            },
+            ...TransitionPresets.SlideFromRightIOS,
+            headerTitle: "",
+          }}
+          component={AccountLevel}
+        />
+         <Stack.Screen
+          name="completeProfile1"
+          options={{
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: "#F2F6FF",
+            },
+            ...TransitionPresets.SlideFromRightIOS,
+            headerTitle: "",
+          }}
+          component={CompleteProfileProcess1}
+        /> 
+        <Stack.Screen
+        name="completeProfile2"
+        options={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "#F2F6FF",
+          },
+          ...TransitionPresets.SlideFromRightIOS,
+          headerTitle: "",
+        }}
+        component={CompleteProfileProcess2}
+      />
       </Stack.Navigator>
     </NavigationContainer>
   );
